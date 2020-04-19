3529ffa7858a8ab169e06e3907332a940062b9dd
riv3rmak3r <kganiga@users.noreply.github.com>
2020-04-19 16:16:35 +0530

    react routing added

--- src/components/container.js
+++ src/components/container.js
@@ -2,13 +2,32 @@ import React from 'react'
 import Card from './card/card'
 import Menu from './sidebar/menu';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import About from './sidebar/help-panel/about/about'
+import Resume from './sidebar/help-panel/resume/resume'
+import Blog from './sidebar/help-panel/blog/blog'
+import Contact from './sidebar/help-panel/contact/contact'
+import Gallery from './sidebar/help-panel/work/work'
 function Container() {
     return (
         <div className="page page_wrap">
             <div className="container opened" data-animation-in="fadeInLeft" data-animation-out="fadeOutLeft">
-            <Menu/>
-            <Card />
-        </div> 
+
+                <BrowserRouter>
+                    <div>
+                        <Menu />
+                        <Switch>
+                            <Route path="/" component={About} exact />
+                            <Route path="/resume" component={Resume} exact />
+                            <Route path="/contact" component={Contact} exact />
+                            <Route path="/blog" component={Blog} exact />
+                            <Route path="/gallery" component={Gallery} exact />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+                <Card />
+            </div>
         </div >
     );
 }
--- src/components/sidebar/menu.js
+++ src/components/sidebar/menu.js
@@ -4,9 +4,11 @@ import MdList from 'react-ionicons/lib/MdList'
 import MdGlobe from 'react-ionicons/lib/MdGlobe'
 import MdCodeWorking from 'react-ionicons/lib/MdImages'
 import MdAt from 'react-ionicons/lib/MdAt'
-import HelpPanel from './help-panel/help-panel'
+
+import { NavLink } from 'react-router-dom';
 
 const cName = 'menu-item menu-item-type-post_type menu-item-object-page menu-item-home page_item page-item-17 current_page_item current-menu-item'
+
 class Menu extends Component {
   constructor(props) {
     super(props);
@@ -36,16 +38,16 @@ class Menu extends Component {
         <div className="top-menu top-menu-onepage">
           <div className="menu-main-menu-container">
             <ul id="menu-main-menu" className="menu">
-              <li onClick={this.getBtnId} id="0" data-id="menu-item-0" className={this.state.panelIndex === 0 ? cName: ''}><a href="#About" className="one-page-menu-item"><span className='icon'><MdPerson /></span>About</a></li>
-              <li onClick={this.getBtnId} id="1" data-id="menu-item-1" className={this.state.panelIndex === 1 ? cName: ''}><a href="#resume" className="one-page-menu-item"><span className='icon'><MdList /></span>Resume</a></li>
-              <li onClick={this.getBtnId} id="2" data-id="menu-item-2" className={this.state.panelIndex === 2 ? cName: ''}><a href="#works" className="one-page-menu-item"><span className='icon'><MdCodeWorking /></span>Gallery</a></li>
-              <li onClick={this.getBtnId} id="3" data-id="menu-item-3" className={this.state.panelIndex === 3 ? cName: ''}><a href="#blog" className="one-page-menu-item"><span className='icon'><MdGlobe /></span>Blog</a></li>
-              <li onClick={this.getBtnId} id="4" data-id="menu-item-4" className={this.state.panelIndex === 4 ? cName: ''}><a href="#contacts" className="one-page-menu-item"><span className='icon'><MdAt /></span>Contact</a></li>
+              <li onClick={this.getBtnId} id="0" data-id="menu-item-0" className={this.state.panelIndex === 0 ? cName: ''}><NavLink to="/"><a href="" className="one-page-menu-item"><span className='icon'><MdPerson /></span>About</a></NavLink></li>
+              <li onClick={this.getBtnId} id="1" data-id="menu-item-1" className={this.state.panelIndex === 1 ? cName: ''}><NavLink to="/resume"><a href="" className="one-page-menu-item"><span className='icon'><MdList /></span>Resume</a></NavLink></li>
+              <li onClick={this.getBtnId} id="2" data-id="menu-item-2" className={this.state.panelIndex === 2 ? cName: ''}><NavLink to="/gallery"><a href="" className="one-page-menu-item"><span className='icon'><MdCodeWorking /></span>Gallery</a></NavLink></li>
+              <li onClick={this.getBtnId} id="3" data-id="menu-item-3" className={this.state.panelIndex === 3 ? cName: ''}><NavLink to="/blog"><a href="" className="one-page-menu-item"><span className='icon'><MdGlobe /></span>Blog</a></NavLink></li>
+              <li onClick={this.getBtnId} id="4" data-id="menu-item-4" className={this.state.panelIndex === 4 ? cName: ''}><NavLink to="/contact"><a href="" className="one-page-menu-item"><span className='icon'><MdAt /></span>Contact</a></NavLink></li>
             </ul>
           </div>
         </div>
       </header>
-      <HelpPanel panelIndex={this.state.panelIndex} />
+      
     </>
     );
   }
