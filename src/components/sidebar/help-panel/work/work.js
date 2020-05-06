ddfdada0eec418243e43387c1c964f8799a95941
riv3rmak3r <kganiga@users.noreply.github.com>
2020-05-06 15:47:22 +0530

    updated pkg json

--- src/components/sidebar/help-panel/work/work.js
+++ src/components/sidebar/help-panel/work/work.js
@@ -8,39 +8,41 @@ import './styles.css'
 class Work extends Component {
 
     render() {
-        return (
+        return (<>
             <div className="card-inner animated active fadeInLeft" id="card-home">
                 <div className="card-wrap">
                     <div className="content works">
                         <div className="title "><span class="first-word">F</span>un Projects</div>
-                        <div className="row grid-items border-line-v" style={{ position: 'relative', height: '1498.83px' }}>
-                            <div className="col col-d-6 col-t-6 col-m-12 border-line-h grid-item f-link " style={{ position: 'absolute', left: '0px', transitionProperty: 'opacity, transform', transitionDuration: '0.4s', transitionDelay: '0ms', top: '0px' }}>
-                                <div className="box-item">
-                                    <div className="image">
-                                        <a href="#number-to-words" className="has-popup-link"><img src={require('../../../../assets/images/numberToStr.jpg')} alt="" />
-                                        </a>
-                                    </div>
-                                    <div className="desc"> <a href="#number-to-words" className="name">Convert Numbers to Words using Indian Numbering</a>
-                                        <div className="category">JavaScript</div>
-                                    </div>
+                        <div className="wrapper">
+                            <div className="card radius shadowDepth1">
+                                <div className="card__image border-tlr-radius">
+                                    <img src={require('../../../../assets/images/numberToStr.jpg')} alt="heh" />
+                                </div>
+                                <div className="card__content card__padding">
+                                    <article className="card__article">
+                                        <h2><a href="#number-to-words">Convert Numbers to Words using Indian Numbering</a></h2>
+                                    </article>
                                 </div>
                             </div>
-                            <div className="col col-d-6 col-t-6 col-m-12 border-line-h grid-item f-gallery " style={{ position: 'absolute', left: '351.175px', top: '0px' }}>
-                                <div className="box-item">
-                                    <div className="image">
-                                        <a href="#read-o-meter" className="has-popup-gallery">
-                                            <img src={require('../../../../assets/images/read-o-meter.jpg')} alt="read o meter" /> </a>
+                            <div className="card radius shadowDepth1">
+                                <div className="card__image border-tlr-radius">
+                                    <img src={require('../../../../assets/images/read-o-meter.jpg')} alt="read o meter" />
+                                </div>
+                                <div className="card__content card__padding">
+                                    <article className="card__article">
+                                        <h2><a href="#read-o-meter">Calculate Reading time</a></h2>
+                                    </article>
+                                </div>
+                            </div>
 
-                                    </div>
-                                    <div className="desc"> <a href="#read-o-meter" className="name">Read o meter</a>
-                                        <div className="category">JavaScript</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="clear" /></div>
+
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+        </>
         )
 
     }
